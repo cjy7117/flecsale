1eba243592fb5f764e65fd98646bb337ef17bf21
Jieyang Chen <cjy7117@gmail.com>
2018-04-14 15:51:12 -0700

    test

---
+++ bee_scalability_test/output_parser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import getpass
+print(getpass.getuser())
+print(getpass.getuser())
+
+import glob
+output_file = open('bee_scalability_test_parsed.output','w')
+output_file.write("Num. of nodes, Proc. per node, Execution time\n")
+path = "bee_scalability_test_*_.output"
+
+for filename in glob.glob(path):
+    parsed_filename = filename.split("_")
+    num_of_node = parsed_filename[len(parsed_filename)-3]
+    proc_per_node = parsed_filename[len(parsed_filename)-2]
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            if "Elapsed wall time" in line:
+                words = line.split(" ")
+                word = words[len(words) - 1]
+                output_file.write("%13d,%15d,%15f\n" % (int(num_of_node), int(proc_per_node), float(word[0:len(word) - 3])))
+output_file.close()
